bcc1889bcd0c1ddd51525750a611b98aeb7721fd
Nicolas <nicolasuliana10@gmail.com>
2024-10-06 02:39:40 -0300

    Commit

--- src/components/PerfilBanner/PerfilBanner.jsx
+++ src/components/PerfilBanner/PerfilBanner.jsx
@@ -1,4 +1,4 @@
-import FotoPerfil from '../../assets/Imagens/para gov.jpeg';
+import FotoPerfil from '../../assets/Imagens/Foto.jpeg';
 import styles from './PerfilBanner.module.css';
 
 export function PerfilBanner(){
@@ -9,7 +9,7 @@ export function PerfilBanner(){
                 <img src={FotoPerfil} className={styles.perfil__img} alt='Foto de Perfil' />
                 <div>
                     <h1 className={styles.perfil__titulo}>Nicolas Uliana Ramos</h1>
-                    <p className={styles.perfil__texto}>Desenvolvedor FullStack - HTML | CSS | Javascript | React JS | Node JS | Express | MVC | MySQL | MongoDB | Sequelize | Git | Java</p>
+                    <p className={styles.perfil__texto}>Desenvolvedor Full Stack / Mobile - HTML | CSS | Javascript | React JS | React Native | Node JS | Express | MVC | MySQL | MongoDB | Sequelize | Git | Java</p>
                 </div>
             </div>
         </div>
