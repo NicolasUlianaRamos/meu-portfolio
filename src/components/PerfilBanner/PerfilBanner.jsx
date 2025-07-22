75f339193f1cf9b20d46ed5812dae39dbd2fc5fe
Nicolas <nicolasuliana10@gmail.com>
2025-07-22 14:05:06 -0300

    new knowledge

--- src/components/PerfilBanner/PerfilBanner.jsx
+++ src/components/PerfilBanner/PerfilBanner.jsx
@@ -9,7 +9,7 @@ export function PerfilBanner(){
                 <img src={FotoPerfil} className={styles.perfil__img} alt='Foto de Perfil' />
                 <div>
                     <h1 className={styles.perfil__titulo}>Nicolas Uliana Ramos</h1>
-                    <p className={styles.perfil__texto}>Desenvolvedor Junior Full Stack | ReactJS, React Native, NodeJS, ExpressJS, C#, ASP.NET, PHP | TypeScript | MySQL, MongoDB, PostgreSQL | APIs RESTful | Java</p>
+                    <p className={styles.perfil__texto}>Desenvolvedor Junior Full Stack | NextJS, ReactJS, React Native, NodeJS, ExpressJS, C#, ASP.NET, PHP | Javascript, TypeScript | MySQL, MongoDB, PostgreSQL | APIs RESTful | Java</p>
                 </div>
             </div>
         </div>
